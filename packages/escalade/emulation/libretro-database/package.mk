e072fd2401d0e71a62560f6b87e92698ab4f8597
Rob Loach <robloach@gmail.com>
2016-08-24 10:18:47 -0400

    libretro-database: update to 5489bcb

--- packages/escalade/emulation/libretro-database/package.mk
+++ packages/escalade/emulation/libretro-database/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libretro-database"
-PKG_VERSION="3090ab3"
+PKG_VERSION="5489bcb"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
